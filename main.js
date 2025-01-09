2d1e54aae01a88610373d200cee38342857f7909
Shalaginov-VD <145598389+Shalaginov-VD@users.noreply.github.com>
2025-01-10 00:06:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const chartData = {
+    labels: ["Python", "C#", "JavaScript"],
+    data: [40, 40, 20],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+    type: "doughnut",
+    data: {
+        labels: chartData.labels,
+        datasets: [
+            {
+                label: "  Знание языка, %",
+                data: chartData.data,
+            },
+        ],
+    },
+    options: {
+        borderWidth: 10,
+        borderRadius: 2,
+        hoverBorderWidth: 0,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+});
+
+const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+        ul.appendChild(li);
+    });
+};
+
+populateUl();
---
+++ validation.js
@@ -0,0 +1,21 @@
+document.getElementById('submitBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!name || !email || !subject || !message) {
+        alert('Пожалуйста, заполните все поля.');
+        return;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert('Пожалуйста, введите корректный адрес электронной почты.');
+        return;
+    }
+
+    alert('Форма успешно отправлена!');
+});
